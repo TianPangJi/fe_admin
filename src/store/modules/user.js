de26bbb470ee1049c74b9a1ec28f391bd03744ec
wangmeng <921781999@qq.com>
2020-09-16 22:50:43 +0800

    修改面包屑导航显示为中文

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -57,7 +57,6 @@ const actions = {
     return new Promise((resolve, reject) => {
       getInfo().then(response => {
         const data = response.data
-        console.log('response.data', response.data)
         if (!data) {
           reject('验证失败，请重新登录')
         }
